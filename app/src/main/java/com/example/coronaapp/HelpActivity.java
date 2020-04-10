09075ef7158812dbc58ff5042960a9ffd89f8574
Rishit Jain <rishit.shree@gmail.com>
2020-04-10 21:12:00 +0530

    initial commit

--- app/src/main/java/com/example/coronaapp/HelpActivity.java
+++ app/src/main/java/com/example/coronaapp/HelpActivity.java
@@ -1,25 +1,45 @@
 package com.example.coronaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 
 public class HelpActivity extends AppCompatActivity {
 
-    private Button site;
+    private static final int REQUEST_PHONE_CALL = 1;
+    private Button site, calling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        calling = (Button) findViewById(R.id.call);
+
+        calling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:011-23978046"));
+                if (ActivityCompat.checkSelfPermission(HelpActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(HelpActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+                }
+                else
+                {
+                    startActivity(callIntent);
+                }
+            }
+        });
     }
     public void open(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://thingspeak.com/channels/977058/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.mygov.in/covid-19/?cbps=1"));
         startActivity(browserIntent);
     }
 }
--- app/src/main/java/com/example/coronaapp/NewsActivity.java
+++ app/src/main/java/com/example/coronaapp/NewsActivity.java
@@ -3,12 +3,19 @@ package com.example.coronaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class NewsActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        webView = findViewById(R.id.webviewid);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSkwyMHZNREZqY0hsNUVnVmxiaTFIUWlnQVAB?hl=en-IN&gl=IN&ceid=IN%3Aen");
     }
 }
